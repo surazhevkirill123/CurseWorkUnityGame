6f0e93dfdc75fb0996815f6c717ca55530b19256
Kirill Surazhev <surazhevkirillleonidovich@gmail.com>
2021-03-17 17:01:38 +0300

    Add source and completed game

---
+++ FlappyBird_v1/New Unity Project/Assets/Scripts/Bird.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Bird : MonoBehaviour
+{
+	// Скорость перемещения
+	public float speed = 2;
+	
+	// Flap force
+	public float force = 300;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Летим в правую сторону
+		GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Взлёт
+		if (Input.GetKeyDown(KeyCode.Space))
+			GetComponent<Rigidbody2D>().AddForce(Vector2.up*force);	
+		if(Input.GetKeyDown(KeyCode.Escape))
+			Application.LoadLevel(0);
+
+    }
+	
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		// Перезапуск
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
---
+++ FlappyBird_v1/New Unity Project/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Направление следования
+	public Transform target;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(target.position.x,
+		transform.position.y,
+		transform.position.z);
+    }
+}
